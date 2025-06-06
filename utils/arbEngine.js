9c695f450ce117de5d2379777c0036935236488d
derekcox4 <derekcox4@gmail.com>
2025-06-05 18:20:51 -0700

    Delete utils directory

--- utils/arbEngine.js
+++
@@ -1,8 +0,0 @@
-
-import { scanDexes } from "./dexStrategy.js";
-import { scanLendingRates } from "./lendingStrategy.js";
-
-export async function runArbitrageStrategies() {
-  await scanDexes();
-  await scanLendingRates();
-}
