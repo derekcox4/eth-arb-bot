d6d3181d191e9e55510b98cc67ddbe22155919bc
derekcox4 <derekcox4@gmail.com>
2025-06-05 17:08:07 -0700

    Update realTimeRunner.js

--- scripts/realTimeRunner.js
+++ scripts/realTimeRunner.js
@@ -1,5 +1,10 @@
 
 require('dotenv').config();
+require('dotenv').config();
+console.log("Checking .env variables...");
+console.log("ALCHEMY_URL loaded:", !!process.env.https://eth-mainnet.g.alchemy.com/v2/tUF1C1jssmen_eIV_K801);
+console.log("WALLET_PRIVATE_KEY loaded:", !!process.env.0de38f52313aebeb186e4ed839380dca4e682177a41aae34a8230f3d435a1a71);
+console.log("FLASHBOTS_RELAY_URL loaded:", !!process.env.https://relay.flashbots.net);
 const { runAllStrategies } = require('../utils/arbEngine');
 console.log("Multi-chain Arbitrage Bot Live...");
 
