c0a8e0183b3b8de6a7b651c60946639b7ffa3809
derekcox4 <derekcox4@gmail.com>
2025-06-05 17:25:38 -0700

    Delete utils/dexStrategy.js

--- utils/dexStrategy.js
+++
@@ -1,7 +0,0 @@
-
-async function scanUniswapArb(chain) {
-  console.log(`[${chain}] Scanning DEX prices for arbitrage...`);
-  // Placeholder for Uniswap/SushiSwap price fetch and profit logic
-}
-
-module.exports = { scanUniswapArb };
