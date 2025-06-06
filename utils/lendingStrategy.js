70c0b1f5d869f29668450e512419e7121b97eeb0
derekcox4 <derekcox4@gmail.com>
2025-06-05 17:47:55 -0700

    Delete utils directory

--- utils/lendingStrategy.js
+++
@@ -1,7 +0,0 @@
-
-async function checkLendingRateArb() {
-  console.log("Checking Aave/Compound for lending rate arbitrage...");
-  // Placeholder for real lending protocol arbitrage checks
-}
-
-module.exports = { checkLendingRateArb };
--- utils/profitLogger.js
+++
@@ -1,7 +0,0 @@
-
-function logProfit() {
-  const timestamp = new Date().toISOString();
-  console.log(`[${timestamp}] Simulated profit: $12.48`);
-}
-
-module.exports = { logProfit };
