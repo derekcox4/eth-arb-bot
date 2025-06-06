4f09551c6de85dc79621d93946fcffe8e5275ecb
derekcox4 <derekcox4@gmail.com>
2025-06-05 18:41:43 -0700

    Delete utils directory

--- utils/arbEngine.js
+++
@@ -1,5 +0,0 @@
-export async function runArbEngine() {
-  console.log("[ethereum] Scanning real DEX prices for arbitrage with execution...");
-  console.log("Current block number: (mock)");
-  console.log("Checking Aave/Compound for lending rate arbitrage...");
-}
